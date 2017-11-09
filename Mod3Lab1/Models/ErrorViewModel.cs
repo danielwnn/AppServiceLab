7809eb7480a982c3a21885d83b16a095be05fdf3
Daniel Wang <nawan@microsoft.com>
2017-11-08 22:40:06 -0800

    dotnet core mvc

---
+++ Mod3Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mod3Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
